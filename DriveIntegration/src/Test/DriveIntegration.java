5c247311e9e85e68df947a3a9f61a78d1ce69f46
mer <meryemgomri@gmail.com>
2020-02-12 21:01:47 +0100

    Mer

--- DriveIntegration/src/Test/DriveIntegration.java
+++ DriveIntegration/src/Test/DriveIntegration.java
@@ -5,6 +5,11 @@
  */
 package Test;
 
+import Core.EventC;
+import Core.OffreC;
+import Utils.Criteres;
+import Utils.Interval;
+
 /**
  *
  * @author Armand
@@ -15,7 +20,20 @@ public class DriveIntegration {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        EventC us = new EventC();
+
+      /*  Criteres critere = new Criteres();
+        critere.ajouterCritere("depart", "Tunis");
+        critere.ajouterCritere("arrivee", "Kairoun");
+        System.out.println(us.filterSelonDesCritere(critere));*/
+     OffreC oc = new OffreC();
+       System.out.println(oc.afficherReduction()); 
+        //System.out.println(oc.afficherPromo());
+      /*  Interval listeInterval = new Interval();
+
+        listeInterval.ajouter("depart", "Tunis", 2);
+        listeInterval.ajouter("arrivee", 15, 16);
+        System.out.println(us.filtrerParInterval(listeInterval));*/
     }
-    
+
 }
