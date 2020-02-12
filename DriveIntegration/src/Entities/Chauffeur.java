6e0cfcf39b00c3ab8b8485cebadc5cb34b749152
Mejbri <skander.mejbri@esprit.tn>
2020-02-12 20:02:38 +0100

    Skander probleme
    s'il te plait armand j'ai un gros pb

--- DriveIntegration/src/Entities/Chauffeur.java
+++ DriveIntegration/src/Entities/Chauffeur.java
@@ -16,18 +16,31 @@ public class Chauffeur {
     private String adresse;
     private int cin;
     private int permis;
+    private String nom;
+    private String prenom;
 
     public Chauffeur() {
     }
 
     
-    public Chauffeur( String adresse, int cin, int permis) {
+    public Chauffeur( String adresse, int cin, int permis, String nom, String prenom) {
        
         this.adresse = adresse;
         this.cin = cin;
         this.permis = permis;
+        this.nom = nom;
+        this.prenom = prenom;
+
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+     
     public int getId_user() {
         return id_user;
     }
@@ -60,10 +73,20 @@ public class Chauffeur {
         this.permis = permis;
     }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
     @Override
     public String toString() {
-        return "Chauffeur{" + "id_user=" + id_user + ", adresse=" + adresse + ", cin=" + cin + ", permis=" + permis + '}';
+        return "Chauffeur{" + "id_user=" + id_user + ", adresse=" + adresse + ", cin=" + cin + ", permis=" + permis + ", nom=" + nom + ", prenom=" + prenom + '}';
     }
+
+   
    
     
 
--- DriveIntegration/src/Test/DriveIntegration.java
+++ DriveIntegration/src/Test/DriveIntegration.java
@@ -5,9 +5,18 @@
  */
 package Test;
 
+import Core.ChauffeurC;
+import Entities.Chauffeur;
+import Entities.Taxi;
+import Utils.DataSource;
+import Core.TaxiC;
+import Utils.Criteres;
+import Utils.Interval;
+
+
 /**
  *
- * @author Armand
+ * @author Belgaroui Ghazi
  */
 public class DriveIntegration {
 
@@ -15,7 +24,52 @@ public class DriveIntegration {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
+         DataSource ds =  DataSource.getInstance();
+          System.out.println(ds.hashCode());
         
+          
+          Chauffeur p = new Chauffeur("ghazella",66,21,"skander","mejbri");
+   ChauffeurC ps = new ChauffeurC();
+   /*ps.ajouterChauffeur(p);
+   System.out.println(ps.afficher());
+   ps.modifierChauffeur(1,"Ariana",287,55);
+       System.out.println(ps.afficher());
+        ps.supprimerChauffeur(1);
+      System.out.println(ps.afficher()+"deleted");*/
+          
+          Taxi t = new Taxi(25,"dgsd","skander");
+   TaxiC pt = new TaxiC();
+   //pt.ajouterTaxi(t);
+   //System.out.println(pt.afficher());
+   //pt.modifierTaxi(8,147,"aaasdg");
+     //  System.out.println(pt.afficher());
+       // pt.supprimerTaxi(8);
+      //System.out.println(pt.afficher()+"deleted");
+          
+      /*    Criteres critere = new Criteres();
+    critere.ajouterCritere("nom", "skander");
+    critere.ajouterCritere("prenom", "ha");
+    System.out.println(ps.filterSelonDesCritere(critere));
+    */
+    /* Interval listeInterval=new Interval();
+     listeInterval.ajouter("id_user",8,9);
+     listeInterval.ajouter("permis",11,21);
+     System.out.println(ps.filtrerParInterval(listeInterval));*/
+     
+        System.out.println(ps.trier("desc", "id_user"));
+        }
+          
+          
+          
+          
+          
+          
+         
+    
+    
+      
+
     }
     
-}
+
